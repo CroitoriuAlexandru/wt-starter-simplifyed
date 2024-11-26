c05e1768af92a6a33b7a3ee64e2666fc291d3bdc
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2024-11-26 22:35:27 +0200

    added proper selection qwestion mark plugin in local plugins and made a copy of it for the ?? preview selection class. it can be configured from tailwind.config.js

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -10,5 +10,22 @@ module.exports = {
     extend: {
     },
   },
-  plugins: [],
+  plugins: [
+    require("./local_plugins/tailwindcss-question-mark-double")({
+      animationDuration: "0.6s",
+      enableAnimation: true,
+      highlightColorStart: "#6BF193",
+      highlightColorEnd: "#1FF760",
+      widthStart: "4px",
+      widthEnd: "6px",
+    }),
+    require("./local_plugins/tailwindcss-question-mark")({
+      animationDuration: "0.6s",
+      enableAnimation: true,
+      highlightColorStart: "#f16bc9",
+      highlightColorEnd: "#f71fb6",
+      widthStart: "4px",
+      widthEnd: "6px",
+    }),
+  ],
 }
